44ad60be8f196f876968cd7af88b4350b547f6fb
Carlos Soto Pérez <carlos348@outlook.com>
2020-03-20 19:37:35 -0600

    User edit versión 2

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let sidebarBtn = document.getElementById('sidebar-btn');
+let sidebar = document.querySelector('.app-sidebar');
+
+sidebarBtn.addEventListener('click', e => {
+    sidebar.classList.toggle('app-sidebar-open');
+    sidebar.classList.toggle('app-sidebar-closed');
+});
