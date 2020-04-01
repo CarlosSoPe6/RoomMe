10d422deb7dbb271b810fc7d6f0b1f2f9720ec7b
Carlos Soto Pérez <carlos348@outlook.com>
2020-04-01 14:15:43 -0600

    Type annotations

--- model/User.js
+++ model/User.js
@@ -3,17 +3,72 @@
 const DBClient = require('./DBClient');
 
 class User extends DBClient {
+
+    /**
+     * @type {number}
+     */
+    Id;
+
+    /**
+     * @type {string}
+     */
+    Name;
+
+    /**
+     * @type {string}
+     */
+    LastName;
+
+    /**
+     * @type {string}
+     */
+    Email;
+
+    /**
+     * @type {string}
+     */
+    Photo;
+
+    /**
+     * @type {string}
+     */
+    PasswordHash;
+
+    /**
+     * @type {string}
+     */
+    GId;
+
+    /**
+     * @type {string}
+     */
+    Phone;
+
+    /**
+     * @type {boolean}
+     */
+    Verified;
+
+    /**
+     * Class constructor
+     */
     constructor(){
         super();
     }
 
+    /**
+     * @returns {User[]} Array of results
+     */
     async getAllUsers() {
         let conn = await this.getConnection();
-        let result = await conn.query('select * from [RoomMe].[User];');
+        let result = await conn.query('select * from [RoomMe].[User];').recordset;
         await this.closeConnection();
         return result;
     }
 }
 
 let test = new User();
-(async () => console.log("USERS:", await test.getAllUsers()))();
+(async () => {
+    let result = await test.getAllUsers();
+    console.log("USERS:", result);
+})();
