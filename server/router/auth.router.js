0b92224c9ce0f7b82d47a7846348145fc8465e30
Carlos Soto Pérez <carlos348@outlook.com>
2020-04-25 14:35:35 -0500

    Comented the tests.

--- server/router/auth.router.js
+++ server/router/auth.router.js
@@ -5,6 +5,6 @@ const User = require('../model/User');
 router.post('/api/login', passportLocal.login);
 
 console.log("Creando usuario...");
-User.createSelfUser(0, "Maki", "La perra", "maki@maki.com", "null", "test", "33333");
+// User.createSelfUser(0, "Maki", "La perra", "maki@maki.com", "null", "test", "33333");
 
 module.exports = router;
