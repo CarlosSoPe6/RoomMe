1e157edd9768949e26f2601aff5cef8b8364e644
Carlos Soto Pérez <carlos348@outlook.com>
2020-04-26 17:51:52 -0500

    Poll starting

--- server/model/Poll.js
+++ server/model/Poll.js
@@ -1,54 +1,114 @@
 'use strict';
 
 const DBClient = require('./DBClient');
-
+const mongoose = require('./../../config/mongo.conf');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 /**
  * Poll model class.
  */
 class Poll extends DBClient {
     
     /**
-     * Id database field.
-     * @type number
-     */
-    Id;
-
-    /**
-     * Title database field.
-     * @type string
-     */
-    Title;
-
-    /**
-     * Description database field.
-     * @type string
-     */
-    Descrption;
-
-    /**
-     * CreatedBy database field. FK to user.
-     * @type number
-     */
-    CreatedBy;
-
-    /**
-     * HouseId database field. FK to House
-     * @type number
-     */
-    HouseId;
-
-    /**
-     * CreatedAt database field.
-     * @type Date
-     */
-    CreatedAt;
-
-
-    /**
      * Constructor method.
      */
     constructor() {
         super();
+
+        this._schema = new mongoose.Schema({
+            uid: {
+                type: Number,
+                unique: true
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            descrption: {
+                type: String,
+                required: true
+            },
+            createdBy: {
+                type: Number,
+                required: true
+            },
+            houseId: {
+                type: Number,
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                required: true,
+                default: Date.now
+            },
+            questions:[
+                {
+                    question: String,
+                    options: Array
+                }
+            ]
+        });
+        this._schema.plugin(AutoIncrement, {inc_field : 'uid'});
+        this._model = mongoose.model('poll', this._schema);
+        /**
+         * Id database field.
+         * @type number
+         */
+        this.uid;
+
+        /**
+         * Title database field.
+         * @type string
+         */
+        this.title;
+
+        /**
+         * Description database field.
+         * @type string
+         */
+        this.descrption;
+
+        /**
+         * CreatedBy database field. FK to user.
+         * @type number
+         */
+        this.createdBy;
+
+        /**
+         * HouseId database field. FK to House
+         * @type number
+         */
+        this.houseId;
+
+        /**
+         * CreatedAt database field.
+         * @type Date
+         */
+        this.createdAt;
+
+        /**
+         * Questons array.
+         * @type PollQuestion[]
+         */
+        this.questions;
+    }
+
+    create(title, descrption, createdBy, houseId, createdAt, questions) {
+        let record = new this._model({
+            title,
+            descrption,
+            createdBy,
+            houseId,
+            createdAt,
+            questions
+        });
+        return await super.add(record);
+    }
+}
+
+class PollQuestion {
+    constructor() {
+        this.question;
+        this.options;
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const mongo = require('./../config/mongo.conf');
 
 const userRouter = require('./router/user.router');
 const authRouter = require('./router/auth.router')
+const contactRouter = require('./router/contact.router');
+const pollRouter = require('./router/poll.router');
 
+app.use('/contact', contactRouter);
+app.use('/poll', pollRouter);
 app.use('/user', userRouter);
 app.use('/', authRouter);
 
