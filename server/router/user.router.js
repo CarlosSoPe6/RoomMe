6a4641a24fc22aa492ee9b4460c2c7e02a3d15c1
Jose Luis Torrentera <jltorrenterash@gmail.com>
2020-04-26 23:03:29 -0500

    Update house user

--- server/router/user.router.js
+++ server/router/user.router.js
@@ -7,6 +7,7 @@ router.get('/', controller.getAllUsers);
 
 router.get('/me', controller.getMe);
 router.put('/me', controller.updateUser);
+router.put('/me/house', controller.updateUserHouse);
 
 router.get('/:id', controller.getUser);
 router.get('/:id/contacts', controller.getContacts);
