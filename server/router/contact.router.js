69ca8fdee88df9ef2ce3f7a5dea81de5159dfb99
Carlos Soto Pérez <carlos348@outlook.com>
2020-04-27 14:13:21 -0500

    RequireAuth middleware.

--- server/router/contact.router.js
+++ server/router/contact.router.js
@@ -2,11 +2,12 @@
 
 const router = require('express').Router();
 const controller = require('./../controllers/contact.controller');
+const requireAuth = require('./../middlewares/requireAuth');
 
-router.get('/', controller.getAllContacts);
-router.post('/', controller.addContact);
+router.get('/', requireAuth, controller.getAllContacts);
+router.post('/', requireAuth, controller.addContact);
 
-router.patch('/:id', controller.updateContact);
-router.delete('/:id', controller.deleteContact);
+router.patch('/:id', requireAuth, controller.updateContact);
+router.delete('/:id', requireAuth, controller.deleteContact);
 
 module.exports = router;
