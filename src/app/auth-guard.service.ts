5edf47f6e25dc7a004c7135f890ac6ebc5315b24
Omar Antonio Madriz Almanza <omar.madriz@OmarMadrizs-MacBook-Pro.local>
2020-05-09 22:39:46 -0500

    Auth service casi terminado

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): boolean {
+    if(!this.authService.isLoggedIn()) {
+      this.router.navigateByUrl('/');
+      return false;
+    }
+    return true;
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -35,4 +35,8 @@ export class AuthService {
       return null;
     }
   }
+
+  public googleLogin(params) {
+
+  }
 }
