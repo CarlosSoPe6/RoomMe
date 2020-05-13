02435e776eddbd3a2a370dde4817897ca53c8a6e
Carlos Soto Pérez <carlos348@outlook.com>
2020-05-12 21:47:16 -0500

    user and poll components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IndexComponent } from './index/index.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { CreateComponent } from './poll/create/create.component';
+import { ListComponent } from './poll/list/list.component';
+import { AnswerComponent } from './poll/answer/answer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     IndexComponent,
-    SidebarComponent
+    SidebarComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    CreateComponent,
+    ListComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule,
