b620ffcfad9db095b4b4c1ddade460dae5da8f1f
Carlos Soto Pérez <carlos348@outlook.com>
2020-11-20 22:26:15 -0600

    Test ADD

--- server/controllers/house.controller.js
+++ server/controllers/house.controller.js
@@ -34,17 +34,13 @@ class HouseControl {
         }
         try{
             const created = await house.addHouse(newHouse);
-            let house_owner = await house.getHouseByOwner(newHouse.ownerId);
-            await user.updateUser(newHouse.ownerId,
-                {
-                    house: house_owner.hid
-                });
             const owner = await user.getSingleUser({'uid': req.user.uid});
             owner.houses.push(created.hid);
             user.updateUser(req.user.uid, {
                 houses: owner.houses,
             });
-            res.send(200).json({hid: house_owner.hid});
+            console.log('POST /house', {hid: created.hid});
+            res.send(200).json({hid: created.hid});
         }
         catch(err){
             console.log(err)
