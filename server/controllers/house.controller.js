627d26cd09798accd24f505b772244fce81bbca0
Carlos Soto Pérez <carlos348@outlook.com>
2020-11-20 20:18:47 -0600

    TEst

--- server/controllers/house.controller.js
+++ server/controllers/house.controller.js
@@ -30,14 +30,14 @@ class HouseControl {
             services: req.body.services
         }
         try{
-            await house.addHouse(newHouse);
+            const created = await house.addHouse(newHouse);
             let house_owner = await house.getHouseByOwner(newHouse.ownerId);
             await user.updateUser(newHouse.ownerId,
                 {
                     house: house_owner.hid
                 });
             const owner = await user.getSingleUser({'uid': req.user.uid});
-            owner.houses.push(house_owner.hid);
+            owner.houses.push(created.hid);
             user.updateUser(req.user.uid, {
                 houses: owner.houses,
             });
